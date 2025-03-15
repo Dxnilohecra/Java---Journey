c2d5d6dcd85e7d7782ccd9ec473203b25ce7fd65
Dxnil <162758713+danilo192836@users.noreply.github.com>
2025-03-15 02:52:07 -0300

    aula6.java
    aula6.java

---
+++ Allproducts.java
@@ -0,0 +1,12 @@
+package aplication;
+
+public class Allproducts {
+    public double feedback = 4.7;
+    public double averageprice = 20.8;
+    public String opinion = "BOA";
+    public String toString() {
+        return "feedback: " + feedback + ", "
+        + "preço médio: " + averageprice + ", " +
+        "opinião média: " + opinion;
+    }
+}
