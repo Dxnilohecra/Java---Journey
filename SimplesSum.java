b47d9356fb59ebf2517e66286f9f6ca574b44294
Dxnil <162758713+Dxnilohecra@users.noreply.github.com>
2025-03-26 01:27:45 -0300

    Aula 8

---
+++ SimplesSum.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class SimplesSum {
+
+    private double num;
+
+    public SimplesSum(double num) {
+        this.num = num;
+    }
+    public double getNum() {
+        return num;
+    }
+    
+    public void setPrice(double num) {
+        this.num = num;
+    }
+}
