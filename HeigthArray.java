6fcd5a0bbe2e6047efcb537d1b106943d77bdacd
Dxnil <162758713+Dxnilohecra@users.noreply.github.com>
2025-03-26 02:11:57 -0300

    Aula 8

---
+++ HeigthArray.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class SimplesSum {
+
+    private double num;
+
+    public SimplesSum(double num) {
+        this.num = num;
+    }
+    
+    public double getNum() {
+        return num;
+    }
+    
+    public void setPrice(double num) {
+        this.num = num;
+    }
+}
