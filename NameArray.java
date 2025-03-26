50c0b69a271faf1a2e57a5f1e8f864d8c762021d
Dxnil <162758713+Dxnilohecra@users.noreply.github.com>
2025-03-26 02:12:15 -0300

    Aula 8

---
+++ NameArray.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class NameArray {
+
+    private String name;
+
+    public NameArray(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
