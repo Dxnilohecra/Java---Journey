3a0cb12368118e0e38f8c570fd33827ad8f826d4
Dxnil <162758713+Dxnilohecra@users.noreply.github.com>
2025-03-26 01:18:05 -0300

    Aula 8

---
+++ Negatives.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Values {
+
+    private double num;
+
+    public Values(double num) {
+        this.num = num;
+    }
+    public double getNum() {
+        return num;
+    }
+    
+    public void setPrice(double num) {
+        this.num = num;
+    }
+}
