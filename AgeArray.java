97663a5ff074c6baeb01c80cceb2f2e37a01253a
Dxnil <162758713+Dxnilohecra@users.noreply.github.com>
2025-03-26 02:11:35 -0300

    Aula 8

---
+++ AgeArray.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class ListAge {
+    private int years;
+
+    public ListAge(int years) {
+        this.years = years;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+}
